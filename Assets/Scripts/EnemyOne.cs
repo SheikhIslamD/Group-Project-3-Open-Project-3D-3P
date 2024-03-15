457af62db6b4ec69db1d709835e311bae6b90f39
Andrew Mokhtary <akouroshm@gmail.com>
2024-03-14 23:32:59 -0400

    Added player and enemy

---
+++ Assets/Scripts/EnemyOne.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyOne : MonoBehaviour
+{
+    NavMeshAgent enemy;
+    GameObject player;
+
+    void Start()
+    {
+        enemy=GetComponent<NavMeshAgent>();
+        player=GameObject.FindWithTag("Player");
+    }
+    void Update()
+    {
+        enemy.SetDestination(player.transform.position);
+    }
+}
