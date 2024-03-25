f50212e40c0d393790176486fbd19541b647378c
Andrew Mokhtary <akouroshm@gmail.com>
2024-03-25 17:00:25 -0400

    WIP Enemy 2

---
+++ Assets/Scripts/Actors/EnemyTwo.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyTwo : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public float attackRange;
+    public bool playerInAttackRange;
+    public GameObject projectile;
+
+    private void Update()
+    {
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+    }
+
+    private void Awake()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+
+        if (!playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange) AttackPlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        enemy.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        enemy.SetDestination(transform.position);
+        transform.LookAt(player);
+        if (!alreadyAttacked)
+        {
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+}
+
