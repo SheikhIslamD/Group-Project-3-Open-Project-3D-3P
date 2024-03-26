0af6e7332d4f4e53d86027ee9e87cb3b82297a72
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-03-26 18:28:18 -0400

    Melee Parry Implementation

--- Assets/Scripts/Actors/PlayerMelee.cs
+++ Assets/Scripts/Actors/PlayerMelee.cs
@@ -6,35 +6,65 @@ public class PlayerMelee : MonoBehaviour
 {
     GameplayInputReader input;
     PlayerShooter shooter;
-    float slashDistance;
+    [SerializeField] float slashDistance;
     [SerializeField] Transform slashTransform;
-    [SerializeField] Collider slashCollider;
+    [SerializeField] float slashRadius;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] int damage = 40;
+    [SerializeField] GameObject slashEcho;
 
 
     private void Start()
     {
         input = GameplayInputReader.Get();
         shooter = GetComponent<PlayerShooter>();
-        slashDistance = slashTransform.localPosition.z;
+        slashEcho.transform.localScale = Vector3.one * slashRadius;
     }
 
     void Update()
     {
-        slashTransform.localPosition = slashDistance * (Vector2)shooter.aimDirection;
+        Vector3 vagueDirection = new Vector3(shooter.aimDirection.x, 0, shooter.aimDirection.z).normalized;
+        slashTransform.position = transform.position + (slashDistance * vagueDirection);
 
-        if (input.melee.WasPressedThisFrame())
+        
+        slashEcho.SetActive(input.melee.IsPressed());
+        if (input.melee.IsPressed())
         {
+            //Invoke("StopSlash", 0.1f);
 
+            RaycastHit hit;
+            Physics.SphereCast(transform.position, slashRadius-1, slashDistance * vagueDirection, out hit, layerMask);
+
+            if (hit.collider == null) return;
+
+            Health health = hit.collider.GetComponent<Health>();
+            if (health != null) health.Damage(damage, Health.DamageType.Melee);
+
+            ReflectableProjectile reflect = hit.collider.GetComponent<ReflectableProjectile>();
+            if (reflect != null)
+            {
+                Debug.Log("Hit Projectile");
+
+                Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+                rb.velocity = Vector3.zero;
+                Vector3 direction = reflect.sender.position - rb.position;
+                rb.AddForce(direction * 1000);
+                reflect.MakeReflected();
+            }
         }
-        else
-        {
-
-        }
 
 
 
 
 
+
+    }
+
+    void StopSlash() => slashEcho.SetActive(false);
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawWireSphere(transform.position + (slashDistance * new Vector3(shooter.aimDirection.x, 0, shooter.aimDirection.z).normalized), slashRadius);
     }
 
 }
---
+++ Assets/Scripts/Objects/ReflectableProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class ReflectableProjectile : MonoBehaviour
+{
+    public Transform sender => GetComponent<PoolableObject>().pool.transform;
+    [SerializeField] LayerMask normalMask;
+    [SerializeField] LayerMask reflectedMask;
+    bool isReflected;
+
+    public void MakeReflected()
+    {
+        GetComponent<Collider>().excludeLayers = reflectedMask;
+        isReflected = true;
+    }
+
+    public void MakeNormal()
+    {
+        GetComponent<Collider>().excludeLayers = normalMask;
+        isReflected = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isReflected)
+        {
+            MakeNormal();
+        }
+    }
+
+}
