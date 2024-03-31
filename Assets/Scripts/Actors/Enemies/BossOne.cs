8428b05f639446f59b1c1d5478cf8f3032307056
Andrew Mokhtary <akouroshm@gmail.com>
2024-03-31 19:41:20 -0400

    Created Enemy Spawners (WIP)

---
+++ Assets/Scripts/Actors/Enemies/BossOne.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossOne : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent enemy;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    public float timeBetweenAttacks;
+    public float attackRange;
+    public bool playerInAttackRange;
+    ObjectPool pool;
+
+    float timeLeftBeforeAttack;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        enemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        pool = GetComponent<ObjectPool>();
+        timeLeftBeforeAttack = timeBetweenAttacks;
+    }
+    
+    private void Update()
+    {
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (!playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange) AttackPlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        enemy.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        enemy.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if(timeLeftBeforeAttack > 0)
+        {
+            timeLeftBeforeAttack -= Time.deltaTime;
+        }
+        else
+        {
+            PoolableObject sphere = pool.Pump();
+            sphere.Prepare_Basic(transform.position, Vector3.zero, transform.forward * 32f + transform.up * 2f);
+
+            /*
+            sphere.gameObject.SetActive(true);
+            sphere.transform.position = transform.position;
+            sphere.transform.eulerAngles = Vector3.zero;
+            Rigidbody rb = sphere.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+             */
+
+            timeLeftBeforeAttack = timeBetweenAttacks;
+        }
+
+    }
+
+    void OnDeplete()
+    {
+        GetComponent<LootBag>().DropLoot(transform.position);
+        Destroy(gameObject);
+    }
+}
