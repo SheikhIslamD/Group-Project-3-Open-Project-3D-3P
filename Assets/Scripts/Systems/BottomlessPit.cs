66f08dbbde00a494a2d1f7916a59e5162b44b0a6
CBowne <156275809+CBowneUCF@users.noreply.github.com>
2024-04-07 15:21:08 -0400

    BottomlessPit Implementation

---
+++ Assets/Scripts/Systems/BottomlessPit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class BottomlessPit : MonoBehaviour
+{
+    Vector3 storedPosition;
+    Transform playerTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<PlayerMove>() != null)
+        {
+            storedPosition = other.transform.position;
+            playerTransform = other.transform;
+        }
+    }
+
+    public void Return()
+    {
+        playerTransform.GetComponent<CharacterController>().Move(storedPosition - playerTransform.transform.position);
+        playerTransform.GetComponent<Health>().Damage(25, Health.DamageType.Generic, this, "BottomlessPit");
+    }
+}
