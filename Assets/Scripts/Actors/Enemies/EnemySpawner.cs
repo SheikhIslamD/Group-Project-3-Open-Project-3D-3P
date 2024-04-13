b1f5eedf8c455a06f95fd6886e235af2623b2e25
CBowne <156275809+CBowneUCF@users.noreply.github.com>
2024-04-13 15:58:59 -0400

    Enemy Spawner Fix

--- Assets/Scripts/Actors/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Actors/Enemies/EnemySpawner.cs
@@ -1,41 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : EnemyBase
 {
     //Config
     [SerializeField] private float spawnRange = 10;
-    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private float spawnRate = 5f;
     [SerializeField] private int maxEnemies = 12;
-    [SerializeField] private GameObject[] enemyPrefabs;
+    //[SerializeField] private GameObject[] enemyPrefabs;
     [SerializeField] private bool canSpawn = true;
 
     //Components
-    ObjectPool[] objectPools;
+    private ObjectPool[] objectPools;
 
     //Data
-    float spawnTimer;
+    private float spawnTimer;
 
     protected override void Awake()
     {
         base.Awake();
-        
-        objectPools = new ObjectPool[enemyPrefabs.Length];
-        for (int i = 0; i < objectPools.Length; i++) objectPools[i] = ObjectPool.Create(gameObject, enemyPrefabs[i], 1, deleteObjectsOnDestroy: false);
 
+        objectPools = GetComponents<ObjectPool>();
+        /*
+        objectPools = new ObjectPool[enemyPrefabs.Length];
+        for (int i = 0; i < enemyPrefabs.Length; i++)
+        {
+            GameObject baseObj = Instantiate(enemyPrefabs[i]);
+            baseObj.transform.parent = transform;
+            baseObj.SetActive(false);
+            objectPools[i] = ObjectPool.Create(gameObject, baseObj, 2, deleteObjectsOnDestroy: false);
+        }
+         */
+        if(objectPools.Length<1) gameObject.SetActive(false);
     }
 
     private void Update()
     {
         if (distanceFromPlayer > spawnRange) return;
-        
-        if(spawnTimer < spawnRate) spawnTimer = Time.deltaTime;
-        if(spawnTimer >= spawnRate)
+
+        if (spawnTimer < spawnRate) spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnRate)
         {
             spawnTimer = 0;
-            if (TotalEnemyCount() < maxEnemies)
-                Spawn();
+            if (TotalEnemyCount() < maxEnemies) Spawn();
         }
 
     }
@@ -56,15 +62,15 @@ public class EnemySpawner : EnemyBase
         }
     }
      */
-    
-    void Spawn()
+
+    private void Spawn()
     {
-        int rand = Random.Range(0, enemyPrefabs.Length);
-        var enemyToSpawn = objectPools[rand].Pump();
+        int rand = Random.Range(0, objectPools.Length);
+        PoolableObject enemyToSpawn = objectPools[rand].Pump();
         enemyToSpawn.transform.position = transform.position - Vector3.forward;
     }
 
-    int TotalEnemyCount()
+    private int TotalEnemyCount()
     {
         int count = 0;
         for (int i = 0; i < objectPools.Length; i++)
